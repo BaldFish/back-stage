6ab4bb62c35edd0deee992ec94f918c5f5393568
lwc <wence.lv@cnlaunch.com>
2018-05-22 13:41:49 +0800

    修改打包相对路径，解决富文本上传图片bug

--- config/index.js
+++ config/index.js
@@ -42,8 +42,9 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    //assetsSubDirectory: 'static',
+    assetsSubDirectory: './',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
